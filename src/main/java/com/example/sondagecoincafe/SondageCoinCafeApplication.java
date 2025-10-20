6dbc3b1ef0796b5a3c69404063c3d48e396a5cc6
Antoine Voituron <avoitu@orange.fr>
2025-10-20 23:06:57 +0200

    [EDIT] first commit creating porject

---
+++ src/main/java/com/example/sondagecoincafe/SondageCoinCafeApplication.java
@@ -0,0 +1,13 @@
+package com.example.sondagecoincafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SondageCoinCafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SondageCoinCafeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sondagecoincafe/SondageCoinCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sondagecoincafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SondageCoinCafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
