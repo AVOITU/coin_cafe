2ecbbbd2b7513ca693a59c00038a03296c097db9
Antoine Voituron <avoitu@orange.fr>
2025-10-28 19:28:53 +0100

    [EDIT] interface dao for result with more inforamtions

--- src/main/java/com/example/sondagecoincafe/dal/ResultDao.java
+++ src/main/java/com/example/sondagecoincafe/dal/ResultDao.java
@@ -1,4 +1,15 @@
 package com.example.sondagecoincafe.dal;
 
+import com.example.sondagecoincafe.bo.Category;
+import com.example.sondagecoincafe.bo.Item;
+
+import java.util.List;
+
 public interface ResultDao {
+
+    Category findCategoryById(int noCategory);
+    void create(Item item);
+    Item read(int no_article);
+    List<Item> findAll();
+    String findName(int no_article);
 }
--- src/main/java/com/example/sondagecoincafe/dal/impl/ResultDaoImpl.java
+++ src/main/java/com/example/sondagecoincafe/dal/impl/ResultDaoImpl.java
@@ -1,8 +1,36 @@
 package com.example.sondagecoincafe.dal.impl;
 
+import com.example.sondagecoincafe.bo.Category;
+import com.example.sondagecoincafe.bo.Item;
 import com.example.sondagecoincafe.dal.ResultDao;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public class ResultDaoImpl implements ResultDao {
+    @Override
+    public Category findCategoryById(int noCategory) {
+        return null;
+    }
+
+    @Override
+    public void create(Item item) {
+
+    }
+
+    @Override
+    public Item read(int no_article) {
+        return null;
+    }
+
+    @Override
+    public List<Item> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public String findName(int no_article) {
+        return "";
+    }
 }
