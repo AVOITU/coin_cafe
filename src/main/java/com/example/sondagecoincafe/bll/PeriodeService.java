cc48e2721805488a944269496f3dd59c8afb0535
Antoine Voituron <avoitu@orange.fr>
2025-10-28 19:10:36 +0100

    [EDIT] preparing services with Interfaces and impl

---
+++ src/main/java/com/example/sondagecoincafe/bll/PeriodeService.java
@@ -0,0 +1,4 @@
+package com.example.sondagecoincafe.bll;
+
+public interface PeriodeService {
+}
---
+++ src/main/java/com/example/sondagecoincafe/bll/impl/PeriodeServiceImpl.java
@@ -0,0 +1,6 @@
+package com.example.sondagecoincafe.bll.impl;
+
+import com.example.sondagecoincafe.bll.PeriodeService;
+
+public class PeriodeServiceImpl implements PeriodeService {
+}
