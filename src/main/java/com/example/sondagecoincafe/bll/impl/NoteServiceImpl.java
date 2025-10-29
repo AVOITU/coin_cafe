45d5beb3f2110c600a4019d0140a931fb223c84b
Antoine Voituron <avoitu@orange.fr>
2025-10-29 22:03:04 +0100

    [EDIT] editing NoteServiceImpl with methods findAllVoteCount

--- src/main/java/com/example/sondagecoincafe/bll/impl/NoteServiceImpl.java
+++ src/main/java/com/example/sondagecoincafe/bll/impl/NoteServiceImpl.java
@@ -1,6 +1,35 @@
 package com.example.sondagecoincafe.bll.impl;
 
 import com.example.sondagecoincafe.bll.NoteService;
+import com.example.sondagecoincafe.bo.Note;
+import com.example.sondagecoincafe.bo.Result;
+import com.example.sondagecoincafe.dal.NoteDao;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+@Getter @Setter
+@NoArgsConstructor
 public class NoteServiceImpl implements NoteService {
+
+    private NoteDao noteDao;
+
+    public NoteServiceImpl(NoteDao noteDao) {
+        this.noteDao = noteDao;
+    }
+
+    public List <Note> findAllVoteCount(){
+        return noteDao.findAllVoteCount();
+    }
+
+    public List <Integer> getVoteCounts(List <Note> note){
+            List<Integer> voteCounts = note.stream()
+                    .map(Note::getVoteCount)
+                    .toList();
+            return voteCounts;
+    }
 }
