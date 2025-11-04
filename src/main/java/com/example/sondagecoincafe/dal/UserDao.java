b079fa77e9b51639242ec1e4971869b54d8ab547
Antoine Voituron <avoitu@orange.fr>
2025-11-04 17:55:34 +0100

    [WIP] pursuing integration JPA by adapting UserDao

--- src/main/java/com/example/sondagecoincafe/dal/UserDao.java
+++ src/main/java/com/example/sondagecoincafe/dal/UserDao.java
@@ -1,11 +1,10 @@
 package com.example.sondagecoincafe.dal;
 
 import com.example.sondagecoincafe.bo.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserDao {
-    Optional<User> getById(int id);
-    Optional<User> selectByUsername(String username);
-    void insertUser(User user);
+public interface UserDao extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
