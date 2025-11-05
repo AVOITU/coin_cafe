5214bfc3b4305d7ae03bb6cb1701a61990aefd9f
Antoine Voituron <avoitu@orange.fr>
2025-11-05 12:22:04 +0100

    [FIX] making orm work

--- src/main/java/com/example/sondagecoincafe/bll/QuestionService.java
+++ src/main/java/com/example/sondagecoincafe/bll/QuestionService.java
@@ -1,9 +1,12 @@
 package com.example.sondagecoincafe.bll;
 
+import com.example.sondagecoincafe.bo.Question;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public interface QuestionService {
+
+    List<Question> getDtoResults();
 }
--- src/main/java/com/example/sondagecoincafe/controller/impl/ResultControllerImpl.java
+++ src/main/java/com/example/sondagecoincafe/controller/impl/ResultControllerImpl.java
@@ -3,11 +3,14 @@ package com.example.sondagecoincafe.controller.impl;
 import com.example.sondagecoincafe.bll.NoteService;
 import com.example.sondagecoincafe.bll.ResultDtoService;
 import com.example.sondagecoincafe.bll.QuestionService;
+import com.example.sondagecoincafe.bo.Question;
 import com.example.sondagecoincafe.controller.ResultController;
 import com.example.sondagecoincafe.dto.ResultsDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ResultControllerImpl implements ResultController {
@@ -23,7 +26,7 @@ public class ResultControllerImpl implements ResultController {
 //    TODO : décommenter le code quand la BDD sera accessible
     @GetMapping("/results")
     public String updateResults(Model m){
-//        List<Question> results = questionService.getDtoResults();
+        List<Question> results = questionService.getDtoResults();
 //        float averageGlobalRating = questionService.calculateAverageRating(results);
 //        List<String> totalVoteCounts = questionService.getTotalVoteCounts(results);
 //        List<Float> questionGlobalNotations = questionService.getQuestionGlobalNotations(results);
